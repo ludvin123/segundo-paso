2fedd11fcc520d42afe0becabb0d5777e17c3784
ludvin <lsgutierrezmontoya@gmail.com>
2018-04-06 13:27:38 -0500

    corrigiendo erroes2

---
+++ main.js
@@ -0,0 +1,9 @@
+function go(){
+if (document.form.password.value=='ludvin' && document.form.login.value=='ludvin'){
+       document.form.submit();
+   }
+   else{
+        alert("Password no registrado");
+   } 
+}
+<--hola -->
